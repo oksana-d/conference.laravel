2e753c4d451efeea3042671ae4889f228f39125b
Oksana Egorova <oe@albedo.dev>
2019-07-31 12:28:17 +0300

    Added Seeders

---
+++ database/seeds/ProfileTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('profile')->insert([
+            'idProfile' => '1',
+            'idUser' => '1',
+            'company' => 'albedo',
+            'position' => 'Web-developer'
+        ]);
+        DB::table('profile')->insert([
+            'idProfile' => '2',
+            'idUser' => '2',
+            'company' => 'bwt',
+            'position' => 'Web-developer'
+        ]);
+    }
+}
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user')->insert([
+            'idUser' => '1',
+            'firstname' => 'Oksana',
+            'lastname' => 'Egorova',
+            'birthday' => date('1999-01-25'),
+            'reportSubject' => 'Report',
+            'country' => 'UA',
+            'phone' => '+380662326591',
+            'email' => 'oe@albedo.dev',
+        ]);
+        DB::table('user')->insert([
+            'idUser' => '2',
+            'firstname' => 'Alex',
+            'lastname' => 'Pklped',
+            'birthday' => date('1988-12-25'),
+            'reportSubject' => 'Report',
+            'country' => 'UA',
+            'phone' => '+380635026591',
+            'email' => 'exalexedsdc@gmail.com',
+        ]);
+    }
+}
