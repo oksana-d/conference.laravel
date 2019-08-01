0f1a18a45c9a7e6ab3511953d36ba012a3a1c5a9
Oksana Egorova <oe@albedo.dev>
2019-08-01 11:27:27 +0300

    Added the ability to share a link to the form

---
+++ config/share.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'share' => [
+        'link' => 'http://conference.laravel/',
+        'text' => 'Check out this Meetup with SoCal AngularJS!'
+    ]
+];
---
+++ resources/views/share.blade.php
@@ -0,0 +1,13 @@
+<div class="row share">
+    <a href="https://www.facebook.com/sharer/sharer.php?u={{ $link }}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+        <img src="{{asset('img/facebook.jpeg')}}" alt="facebook">
+    </a>
+    <a href="http://twitter.com/share?&url={{ $link }}&text={{ $text }}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+        <img src="{{asset('img/twitter.jpeg')}}" alt="twitter">
+    </a>
+</div>
+<div class="row">
+    <div class="col">
+        <a href="#" class="float-right">All members ({{ $countMembers }})</a>
+    </div>
+</div>
